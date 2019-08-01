ae5c7a540f044bf83fc0beba43c4f5ffbf9b645a
AndreinaKj <51204607+AndreinaKj@users.noreply.github.com>
2019-08-01 11:02:19 -0500

    Agregando información del modal (#13)
    * instalando el package json
    
    * Desplegando informacion en el modal

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 const form = document.getElementById('form-search-movies');
 const searchField = document.getElementById('search-movies');
 const moviesContainer = document.getElementById('movies-container');
+const modalContainer = document.getElementById('modal-container');
 
 const movieData = app.getMoviesFromTMDB(tmdbApiKey);
 
 window.onload = async () => {
-	const results = await movieData;
-	results.slice(0, 10).forEach((movie) => {
-		moviesContainer.innerHTML += `
+    const results = await movieData;
+    results.slice(0, 10).forEach((movie) => {
+        moviesContainer.innerHTML += `
      <div class="card col-md-4" id="${movie.imdb_id}">
      <img src="http://image.tmdb.org/t/p/w185/${movie.poster_path}" class="card-img-top" alt="...">
      <div class="card-body">
@@ -20,37 +21,44 @@ window.onload = async () => {
      </div>
    </div>
     `;
-		createModal();
-	});
+        createModal();
+    });
 };
 
 
 const createModal = () => {
-	const allMovies = document.querySelectorAll('#movies-container > .card');
+    const allMovies = document.querySelectorAll('#movies-container > .card');
 
-	allMovies.forEach((card) => {
-		card.addEventListener('click', async (event) => {
-			const movieId = event.currentTarget.id; // currentTarget obtenemos el id del padre
-			console.log(movieId);
-			const gettingMovieOMDB = await app.getMovieByIMDB(movieId, omdbApiKey);
-			console.log(gettingMovieOMDB);
+    allMovies.forEach((card) => {
+        card.addEventListener('click', async (event) => {
+            const movieId = event.currentTarget.id; // currentTarget obtenemos el id del padre
+            const gettingMovieOMDB = await app.getMovieByIMDB(movieId, omdbApiKey);
+            console.log(gettingMovieOMDB);
+            const {
+                // realizo asignación por destructuring de la data
+                Title,
+                Year,
+                Poster,
+                imdbRating,
+                Production
+            } = gettingMovieOMDB;
 
-			const {
-				// realizo asignación por destructuring de la data
-				Title,
-				Year,
-				Poster,
-				imdbRating,
-				Production
-			} = gettingMovieOMDB;
-
-			$('#movieId').modal('show')
-		});
-	});
+            modalContainer.innerHTML += ` 
+            <div>${Title}</div>
+            <div>${Year}</div>
+            <img src="${Poster}">
+            <div><p>Rating</p>${imdbRating}</div>
+            <div><p>Productora</p>${Production}</div>
+            `;
+            $('#movieId').modal('show')
+        });
+    });
 };
 
+
+
 const showModal = () => {
-	modal.classList.add('visible');
-	mask.classList.add('visible');
-	modalBg.classList.add('visible');
+    modal.classList.add('visible');
+    mask.classList.add('visible');
+    modalBg.classList.add('visible');
 };
